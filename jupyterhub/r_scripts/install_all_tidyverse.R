2a87ee66efa3c1c6a362374dd75d359bfd26d57a
andrew-leith <andrew-leith@users.noreply.github.com>
2019-06-27 15:42:40 -0400

    added tidyverse script and dockerfile

---
+++ jupyterhub/r_scripts/install_all_tidyverse.R
@@ -0,0 +1,3 @@
+install.packages("BiocManager", repos = "http://cran.us.r-project.org")
+install.packages("dplyr", repos = "http://cran.us.r-project.org")
+install.packages("ggplot2", repos = "http://cran.us.r-project.org")
